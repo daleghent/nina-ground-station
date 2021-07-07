5babdb1a3c1d1fb80d6de55ca04cc9a7962eab89
Dale Ghent <daleg@elemental.org>
2021-07-07 13:44:58 -0400

    set minimum nina version to 1.11.0.1106

--- GroundStation/Properties/AssemblyInfo.cs
+++ GroundStation/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2021 Dale Ghent")]
 
 // The minimum Version of N.I.N.A. that this plugin is compatible with
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "1.11.0.1105")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "1.11.0.1106")]
 
 // The license your plugin code is using
 [assembly: AssemblyMetadata("License", "MPL-2.0")]
