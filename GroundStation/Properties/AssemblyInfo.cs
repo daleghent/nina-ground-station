466961b6235038b58d63e29b7a0778823184eb9c
Dale Ghent <daleg@elemental.org>
2021-07-07 11:53:24 -0400

    Add changelog URL

--- GroundStation/Properties/AssemblyInfo.cs
+++ GroundStation/Properties/AssemblyInfo.cs
@@ -43,7 +43,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("Tags", "notifications,alerts,ifttt,email,pushover")]
 
 //[Optional] A link that will show a log of all changes in between your plugin's versions
-[assembly: AssemblyMetadata("ChangelogURL", "")]
+[assembly: AssemblyMetadata("ChangelogURL", "https://github.com/daleghent/nina-plugins/blob/main/GroundStation/CHANGELOG.md")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
 [assembly: AssemblyMetadata("FeaturedImageURL", "https://daleghent.github.io/nina-plugins/assets/images/ground-station-logo1.png")]
