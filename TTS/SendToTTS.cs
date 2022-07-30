e42a08f526d630b0ba49899dd3fa39e7a18cbf69
Stefan Berg <isbeorn86@googlemail.com>
2022-07-31 00:56:36 +0200

    Add a new instruction and failure trigger to send messages to Text-To-Speech

---
+++ TTS/SendToTTS.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using NINA.Core.Model;
+using NINA.Sequencer.SequenceItem;
+using NINA.Sequencer.Validations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DaleGhent.NINA.GroundStation.TTS {
+
+    [ExportMetadata("Name", "Send to TTS")]
+    [ExportMetadata("Description", "Plays a Text-To-Speech announcement from given message")]
+    [ExportMetadata("Icon", "TTS_SVG")]
+    [ExportMetadata("Category", "Ground Station")]
+    [Export(typeof(ISequenceItem))]
+    [JsonObject(MemberSerialization.OptIn)]
+    public class SendToTTS : SequenceItem, IValidatable {
+        private TTS tts;
+        private string message;
+
+        [ImportingConstructor]
+        public SendToTTS() {
+            tts = new TTS();
+        }
+
+        public SendToTTS(SendToTTS copyMe) : this() {
+            CopyMetaData(copyMe);
+        }
+
+        public override object Clone() {
+            return new SendToTTS(this) {
+            };
+        }
+
+        [JsonProperty]
+        public string Message {
+            get => message;
+            set {
+                message = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public override async Task Execute(IProgress<ApplicationStatus> progress, CancellationToken ct) {
+            var text = Utilities.Utilities.ResolveTokens(Message, this);
+
+            await tts.Speak(text, ct);
+        }
+
+        public IList<string> Issues { get; set; } = new List<string>();
+
+        public bool Validate() {
+            var i = new List<string>();
+
+            if (string.IsNullOrEmpty(Message) || string.IsNullOrWhiteSpace(Message)) {
+                i.Add("TTS message is missing");
+            }
+
+            if (!tts.HasVoice()) {
+                i.Add("No Text-To-Speech voices found");
+            }
+
+            if (i != Issues) {
+                Issues = i;
+                RaisePropertyChanged("Issues");
+            }
+
+            return i.Count == 0;
+        }
+
+        public override string ToString() {
+            return $"Category: {Category}, Item: {nameof(SendToTTS)}";
+        }
+    }
+}
