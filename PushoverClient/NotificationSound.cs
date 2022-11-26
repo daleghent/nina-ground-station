0d53ce90e0df0013309da36322fdc41c5d8a0d2c
Dale Ghent <daleg@elemental.org>
2022-11-26 11:16:54 -0500

    CodeMaid the new code

--- PushoverClient/NotificationSound.cs
+++ PushoverClient/NotificationSound.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region "copyright"
+
+/*
+    Copyright Dale Ghent <daleg@elemental.org> and contributors
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/
+*/
+
+#endregion "copyright"
 
 namespace DaleGhent.NINA.GroundStation.PushoverClient {
+
     /// <summary>
     /// https://pushover.net/api#sounds
     /// </summary>
