3e4e7ef823b8d48428446c1d584449fdfdc31390
Dale Ghent <daleg@elemental.org>
2024-04-14 12:40:07 -0400

    Add copyright header

--- Utilities/SelectTextOnFocus.cs
+++ Utilities/SelectTextOnFocus.cs
@@ -1,10 +1,24 @@
-﻿using System.Windows;
+﻿#region "copyright"
+
+/*
+    Copyright (c) 2024 Dale Ghent <daleg@elemental.org>
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/
+*/
+
+#endregion "copyright"
+
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 
 namespace DaleGhent.NINA.GroundStation.Utilities {
+
     public class SelectTextOnFocus : DependencyObject {
+
         public static readonly DependencyProperty ActiveProperty = DependencyProperty.RegisterAttached(
             "Active",
             typeof(bool),
