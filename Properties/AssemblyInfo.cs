ff0cb70fc6bd8b10b2cb5769359560823f360d21
Dale Ghent <daleg@elemental.org>
2025-04-23 00:15:54 -0400

    3.0 beta 8

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using System.Runtime.InteropServices;
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("3.0.0.107")]
-[assembly: AssemblyFileVersion("3.0.0.107")]
+[assembly: AssemblyVersion("3.0.0.108")]
+[assembly: AssemblyFileVersion("3.0.0.108")]
 
 // [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("Ground Station")]
