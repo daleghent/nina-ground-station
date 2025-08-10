6de544647cedce6a44410e1e1425fafcfd9239a1
Dale Ghent <daleg@elemental.org>
2025-08-10 00:04:26 -0400

    Convert Send to MQTT to configuration window

--- MQTT/SendToMqtt.cs
+++ MQTT/SendToMqtt.cs
@@ -10,11 +10,14 @@
 
 #endregion "copyright"
 
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using DaleGhent.NINA.GroundStation.MetadataClient;
 using DaleGhent.NINA.GroundStation.Mqtt;
 using Newtonsoft.Json;
 using NINA.Core.Model;
 using NINA.Core.Utility;
+using NINA.Core.Utility.WindowService;
 using NINA.Equipment.Interfaces.Mediator;
 using NINA.Sequencer.SequenceItem;
 using NINA.Sequencer.Validations;
@@ -33,7 +36,7 @@ namespace DaleGhent.NINA.GroundStation.SendToMqtt {
     [ExportMetadata("Category", "Ground Station")]
     [Export(typeof(ISequenceItem))]
     [JsonObject(MemberSerialization.OptIn)]
-    public class SendToMqtt : SequenceItem, IValidatable {
+    public partial class SendToMqtt : SequenceItem, IValidatable {
         private string topic;
         private string payload = string.Empty;
         private int qos = 0;
@@ -52,6 +55,7 @@ namespace DaleGhent.NINA.GroundStation.SendToMqtt {
         private readonly IWeatherDataMediator weatherDataMediator;
 
         private readonly IMetadata metadata;
+        private IWindowService windowService;
 
         [ImportingConstructor]
         public SendToMqtt(ICameraMediator cameraMediator,
@@ -113,6 +117,8 @@ namespace DaleGhent.NINA.GroundStation.SendToMqtt {
             set {
                 topic = value;
                 RaisePropertyChanged();
+                RaisePropertyChanged(nameof(MessagePreview));
+                Validate();
             }
         }
 
@@ -122,6 +128,8 @@ namespace DaleGhent.NINA.GroundStation.SendToMqtt {
             set {
                 payload = value;
                 RaisePropertyChanged();
+                RaisePropertyChanged(nameof(MessagePreview));
+                Validate();
             }
         }
 
@@ -144,6 +152,24 @@ namespace DaleGhent.NINA.GroundStation.SendToMqtt {
         }
 
         public static IList<string> QoSLevels => MqttCommon.QoSLevels;
+
+        public string MessagePreview {
+            get {
+                string text = string.Empty;
+                byte mesgPreviewLen = 50;
+
+                if (!string.IsNullOrEmpty(payload)) {
+                    var count = payload.Length > mesgPreviewLen ? mesgPreviewLen : payload.Length;
+                    text = payload[..count];
+
+                    if (payload.Length > mesgPreviewLen) {
+                        text += "...";
+                    }
+                }
+
+                return text;
+            }
+        }
 
         public override async Task Execute(IProgress<ApplicationStatus> progress, CancellationToken ct) {
             var payload = Utilities.Utilities.ResolveTokens(Payload, this, metadata);
@@ -181,5 +207,50 @@ namespace DaleGhent.NINA.GroundStation.SendToMqtt {
         public override string ToString() {
             return $"Category: {Category}, Item: {Name}, Topic: {Topic}, QoS: {QoS}, Retain: {Retain}, PayloadLength={Payload.Length}";
         }
+
+        public IWindowService WindowService {
+            get {
+                windowService ??= new WindowService();
+                return windowService;
+            }
+
+            set => windowService = value;
+        }
+
+        // This attribute will auto generate a RelayCommand for the method. It is called <methodname>Command -> OpenConfigurationWindowCommand. The class has to be marked as partial for it to work.
+        [RelayCommand]
+        private async Task OpenConfigurationWindow(object o) {
+            var conf = new SendToMqttSetup() {
+                Topic = this.Topic,
+                Payload = this.Payload,
+                QoS = this.QoS,
+                Retain = this.Retain
+            };
+
+            await WindowService.ShowDialog(conf, Name, System.Windows.ResizeMode.CanResize, System.Windows.WindowStyle.ThreeDBorderWindow);
+
+            Topic = conf.Topic;
+            Payload = conf.Payload;
+            QoS = conf.QoS;
+            Retain = conf.Retain;
+        }
     }
+
+    public partial class SendToMqttSetup : BaseINPC {
+        [ObservableProperty]
+        private string topic;
+
+        [ObservableProperty]
+        private string payload;
+
+        [ObservableProperty]
+        private int qoS;
+
+        [ObservableProperty]
+        private bool retain;
+
+        [ObservableProperty]
+        private IList<string> qoSLevels = MqttCommon.QoSLevels;
+    }
+
 }
